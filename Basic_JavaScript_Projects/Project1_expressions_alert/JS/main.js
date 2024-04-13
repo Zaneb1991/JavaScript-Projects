52c2b5ebe8c0f249d09e354dbc0747fa4b9c4b97
Zaneb1991 <zaneb123@hotmail.co.uk>
2024-04-13 22:14:09 +0100

    Create main.js

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+//window.alert
+window.alert("Hello all you amazing people");
+
+//document.write
+document.write("Hope you are doing well");
+
+//creating variables
+var A = "I want to first start by saying, Welcome";
+
+var B = "I want to end this wishing you a great day!";
+
+//displaying the variables
+document.write(A + B);
+
+//expression
+document.write(152 + 268);
